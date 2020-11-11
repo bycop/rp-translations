0b39445da81f80a88ed883862919b058e3100402
FP <pichotcontact@gmail.com>
2020-11-11 18:16:09 -0500

    ?

---
+++ fr.lua
@@ -0,0 +1,17 @@
+local my_language = {
+	-- poll one
+	poll1_q1 = "Quel genre de serveur souhaiteriez-vous voir?",
+	poll1_q2 = "Quelle mise à jour souhaiteriez-vous prioriser sur Extinction?",
+
+	poll1_q1_a1 = "Battle Royale",
+	poll1_q1_a2 = "Mission privée solo ou avec 1-4 amis (comme Payday / Heist / Hitman)",
+	poll1_q1_a3 = "Course",
+	poll1_q1_a4 = "Murder / Horreur",
+
+	poll1_q2_a1 = "PvE améliorer les zombies/farm",
+	poll1_q2_a2 = "Ajouter des évènements sur la map",
+	poll1_q2_a3 = "Shop/Troc MAJ",
+	poll1_q2_a4 = "Plus de stats RPG / améliorations",
+}
+
+AddLanguage("fr", my_language)
